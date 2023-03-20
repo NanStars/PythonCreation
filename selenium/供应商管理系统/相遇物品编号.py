71c9b0c220288505a830e7797b59d98ef5dc8d11
NanStars <47203927+NanStars@users.noreply.github.com>
2023-03-20 22:46:43 +0800

    更新 相遇物品编号.py

--- selenium/供应商管理系统/相遇物品编号.py
+++ selenium/供应商管理系统/相遇物品编号.py
@@ -3,7 +3,7 @@ from lxml import etree
 
 
 def getHTMLText(url):
-    cookie = "_sy_iu_0201=atmti7e7qmo57gfvaivrymf2ncdkgxg23eymofyajjznj5kufgzevxe627vpheyxqcjueqsdxdpfqhaviiiq3uvqlmnpseqlmrnw4lub2llgusalpzikfjeftriuiy2yzeorkiy2427vsuiemxupjcexji"
+    cookie = "****"
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.2; WOW64; rv:21.0) Gecko/20100101 Firefox/21.0',
         'Cookie': cookie
